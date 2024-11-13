007518263213bede15ca596f52b05cda83de246c
MathTheMith <mathvacprv@gmail.com>
2024-11-13 17:46:44 +0100

    substr norminetted

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,33 @@
 /*   By: math <math@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 18:40:19 by math              #+#    #+#             */
-/*   Updated: 2024/11/12 18:40:52 by math             ###   ########lyon.fr   */
+/*   Updated: 2024/11/13 17:20:47 by math             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_empty_string(void)
+{
+	char	*ptr;
+
+	ptr = malloc(1);
+	if (!ptr)
+		return (NULL);
+	ptr[0] = '\0';
+	return (ptr);
+}
+
+char	*ft_alloc_substr(size_t len)
+{
+	char	*ptr;
+
+	ptr = malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	ptr[len] = '\0';
+	return (ptr);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -21,25 +43,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	str_len = ft_strlen(s);
 	if (start >= str_len)
-	{
-		ptr = malloc(1);
-		if (!ptr)
-			return (NULL);
-		ptr[0] = '\0';
-		return (ptr);
-	}
+		return (ft_empty_string());
 	if (len > str_len - start)
 		len = str_len - start;
-	ptr = malloc(len + 1);
+	ptr = ft_alloc_substr(len);
 	if (!ptr)
 		return (NULL);
 	while (i < len && s[start])
-	{
-		ptr[i] = s[start];
-		i++;
-		start++;
-	}
-	ptr[i] = '\0';
+		ptr[i++] = s[start++];
 	return (ptr);
 }
 
