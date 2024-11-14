a574196c5f981f55b2deb5f369fee5568806ea2b
MathTheMith <mathvacprv@gmail.com>
2024-11-14 11:32:21 +0100

    added lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,14 +6,28 @@
 /*   By: math <math@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:16:54 by mvachon           #+#    #+#             */
-/*   Updated: 2024/11/13 17:48:08 by math             ###   ########lyon.fr   */
+/*   Updated: 2024/11/14 11:31:54 by math             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-// t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list *new;
-// 	char *ptr;
-// 	ptr = ft_striteri(ptr, f)
-// 	if
-// }
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*new_element;
+
+	new = NULL;
+	while (lst)
+	{
+		new_element = ft_lstnew(f(lst->content));
+		if (!new_element)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, new_element);
+		lst = lst->next;
+	}
+	return (new);
+}
